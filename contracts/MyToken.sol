801ca3f4e5c85cb7bedfbdb24f4c31389971f664
obddd <obaidulhaq20@gmail.com>
2021-09-12 01:00:26 +0500

    wrote unit tests and smart contract to transfer token

---
+++ contracts/MyToken.sol
@@ -0,0 +1,16 @@
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+contract MyToken is ERC20 {
+  function decimals() public view virtual override returns (uint8) {
+    return 0;
+  }
+
+  constructor(uint256 initialSupply) ERC20('StarDucks Capu-Token', 'SCT') {
+    _mint(msg.sender, initialSupply);
+    decimals();
+  }
+}
